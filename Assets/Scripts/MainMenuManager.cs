9e90c3bb8b85e142b0f14f4c0dec5f49b6e8aa01
HaitoAsaka <haitoasakalamson@gmail.com>
2025-08-08 15:44:48 +0700

    Update 8/8
    add some texture and volume bar

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // Thêm dòng này
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class MainMenuManager : MonoBehaviour
 {
     public GameObject mainMenuPanel;
     public GameObject settingPanel;
 
+    [Header("Âm thanh")]
+    public Slider volumeSlider;
+    public AudioMixer audioMixer;
+
+    void Start()
+    {
+        // Gán sự kiện thay đổi volume
+        if (volumeSlider != null)
+        {
+            volumeSlider.onValueChanged.AddListener(SetVolume);
+
+            // Đặt giá trị khởi đầu (ví dụ: 0.5)
+            float defaultVolume = PlayerPrefs.GetFloat("volume", 0.5f);
+            volumeSlider.value = defaultVolume;
+            SetVolume(defaultVolume);
+        }
+    }
+
     // Hàm bắt đầu game
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene"); 
+        SceneManager.LoadScene("SampleScene");
     }
 
     // Hiện menu cài đặt
@@ -34,7 +54,16 @@ public class MainMenuManager : MonoBehaviour
         Application.Quit();
 
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false; // Dành cho khi test trong Unity Editor
+        UnityEditor.EditorApplication.isPlaying = false;
 #endif
+    }
+
+    // Hàm chỉnh âm lượng
+    public void SetVolume(float volume)
+    {
+        // Tránh lỗi Log10(0)
+        float volumeDB = Mathf.Log10(Mathf.Clamp(volume, 0.0001f, 1f)) * 20f;
+        audioMixer.SetFloat("Volume", volumeDB);
+        PlayerPrefs.SetFloat("volume", volume); // Lưu lại nếu cần
     }
 }
